cc6b8f44f3bfb5a31652b465952a9fcb951da1bd
QQC <ququanchang@apexsoft.com.cn>
2024-05-16 17:16:39 +0800

    springboot整合WebService demo

---
+++ src/main/java/com/qqc/springbootwebservicedemo/SpringbootWebserviceDemoApplication.java
@@ -0,0 +1,13 @@
+package com.qqc.springbootwebservicedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootWebserviceDemoApplication.class, args);
+    }
+
+}
